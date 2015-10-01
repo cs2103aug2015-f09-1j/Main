effc8b758c9f2b2820baaad7531d121397b85f4d
YiHong <YiHong@LimYiHong.home>
2015-10-02 00:11:30 +0800

    Added Some more content to the code

--- repo/Storage.java
+++ repo/Storage.java
@@ -4,9 +4,23 @@ import java.io.File;
 
 public class Storage {
 	Storage(){
+		
 		System.out.println("Storage is ready.");
+		
 	}
 	public void refreshFile(File file){
 		System.out.println("New version of file is stored.");
+	}
+	
+	public void contentDelete(File file){
+		System.out.println("Content deleted.");
+	}
+	
+	public void contentAdd(File file){
+		System.out.println("Content added.");
+	}
+	
+	public void contentEdit(File file){
+		System.out.println("Content Edited.");
 	}
 }
