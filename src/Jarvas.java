7a11d6041753ea2fb1cca02477dc15f1658cb9e0
Jaime Pabilona <mouth46@hotmail.com>
2015-10-01 22:06:43 +0800

    Testing

--- src/Jarvas.java
+++ src/Jarvas.java
@@ -1,10 +1,14 @@
-//empty
+import java.util.*;
+
 public class Jarvas {
 
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(int test){
 	
 		System.out.println("test");
-		
+		String command = sc.nextLine();
+		System.out.println(command);
 	}
 	
 }
