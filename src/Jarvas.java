6472cd8497b03c049208c12aff2194446b5b8cb6
Jaime Pabilona <mouth46@hotmail.com>
2015-10-01 22:16:44 +0800

    Added stub?

--- src/Jarvas.java
+++ src/Jarvas.java
@@ -5,10 +5,13 @@ public class Jarvas {
 	private static Scanner sc = new Scanner(System.in);
 	
 	public static void main(int test){
-	
 		System.out.println("test");
 		String command = sc.nextLine();
 		System.out.println(command);
+	}
+	
+	private static void storeToFile(String task){
+		//stub		
 	}
 	
 }
