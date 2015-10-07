5fee70fdc9d7bc0990ca54f16fe30bd9231352f6
Nabula Lee <13307130498@fudan.edu.cn>
2015-10-07 21:55:47 +0800

    class Start finished and all APIs defined

--- V0.1/Logic.java
+++ V0.1/Logic.java
@@ -1,15 +1,35 @@
 package repo;
 
+import java.util.Vector;
+
 public class Logic {
 	String command;
 	Logic(){
 		System.out.println("Logic is ready.");
 	}
-	public void getCommand(String str){
-		command = str;
-		System.out.println("Logic is getting the command.");	
+	public void getInput(String str){
+		// TODO str is the input	
 	}
 	public void execute(){
-		System.out.println("Command is executing.");
+		// TODO execute the command
+	}
+	public void getOriginalTasks(Vector<String> returnTasks) {
+		// TODO initialize the vector for tasks
+		
+	}
+	public Vector<String> returnNewTasks() {
+		// TODO return the new vector contains tasks after each operation
+		return null;
+	}
+	public void getOutput(String returnOutput) {
+		// TODO deal with the output from storage
+		
+	}
+	public String returnOutput() {
+		/** TODO return the feedback to ui to print on screen
+		 * (this should contains the feedback from storage, 
+		 * use \n to make a new line in the string)
+		 **/
+		return null;
 	}
 }
--- V0.1/Storage.java
+++ V0.1/Storage.java
@@ -1,26 +1,28 @@
 package repo;
 
 import java.io.File;
+import java.util.Vector;
 
 public class Storage {
 	Storage(){
+		// TODO no filename
+	}
+	public Storage(String str) {
+		// TODO str is filename
+	}
+	public void refreshFile(){
+		// TODO save the new Vector into the file
+	}
+	public Vector<String> returnTasks() {
+		// TODO return the vector contains tasks
+		return null;
+	}
+	public void getNewTasks(Vector<String> returnNewTasks) {
+		// TODO get the new tasks for storing into the file
 		
-		System.out.println("Storage is ready.");
-		
 	}
-	public void refreshFile(File file){
-		System.out.println("New version of file is stored.");
-	}
-	
-	public void contentDelete(File file){
-		System.out.println("Content deleted.");
-	}
-	
-	public void contentAdd(File file){
-		System.out.println("Content added.");
-	}
-	
-	public void contentEdit(File file){
-		System.out.println("Content Edited.");
+	public String returnOutput() {
+		// TODO return the feedback to logic
+		return null;
 	}
 }
