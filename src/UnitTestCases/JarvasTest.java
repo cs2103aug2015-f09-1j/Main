2ebef9649a0b81e4de5d7291ea532c4310f470b4
Jaime Pabilona <mouth46@hotmail.com>
2015-11-08 19:18:37 +0800

    Added some unit tests

--- src/UnitTestCases/JarvasTest.java
+++ src/UnitTestCases/JarvasTest.java
@@ -19,35 +19,24 @@ public class JarvasTest {
 	
 	@Test
 	public void testDetermineCommandType(){
-		/*
-		assertEquals(Parser.CommandType.ADDTASK,Parser.determineCommandType("addtask"));
-			//equivalence partitioning for valid command (add,display,clear,exit)
-			assertEquals(Parser.CommandType.ADDTASK,Parser.determineCommandType("add"));
-			assertEquals(Parser.CommandType.DISPLAY,Parser.determineCommandType("display"));
-			assertEquals(Parser.CommandType.CLEAR,Parser.determineCommandType("clear"));
-			assertEquals(Parser.CommandType.EXIT,Parser.determineCommandType("exit"));
-			//equivalence partitioning for invalid command
-			assertEquals(Parser.CommandType.INVALID,Parser.determineCommandType("ola"));
-			*/
+		//Need to add test case		
 	}
 	
 	@Test
 	public void testExecute() throws IOException{
 		//equivalence partitioning for invalid value partition(without command)
-		//assertEquals("invalid input", logic.execute("444"));
-		//assertEquals("invalid input", logic.execute("eeeee"));
+		assertEquals("invalid input", logic.execute("444"));
+		assertEquals("invalid input", logic.execute("eeeee"));
 		//equivalence partitioning for valid value partition (addtask command follow by dueDate)
-		//assertEquals("task \"ola\" successfully added", logic.execute("addtask ola -due 3/3/3/3/3"));
+		assertEquals("task \"ola\" successfully added", logic.execute("addtask ola -due 3/3/3/3/3"));
 
 	}
 	@Test
 	public void testAddEvent(){
-//		// equivalence partitioning with valid date format dd/mm/yyyy HH/MM
-//		assertEquals("event \"ola\" successfully added", 
-//				logic.addEvent("ola -from 12/12/1991 12:12 -to 12/12/1991 12:13"));
-//		//equivalence partitioning with invalid date format
-//		assertEquals("invalid format of date", 
-//				logic.addEvent("ola -from 12/12/1991 12 -to 12/12/1991 12:13"));
+		// equivalence partitioning with valid date format dd/mm/yyyy HH/MM
+		assertEquals("event \"ola\" successfully added", logic.execute("ola -from 12/12/1991 12:12 -to 12/12/1991 12:13"));
+		//equivalence partitioning with invalid date format
+		assertEquals("invalid format of date", logic.execute("ola -from 12/12/1991 12 -to 12/12/1991 12:13"));
 	}
 
 	
